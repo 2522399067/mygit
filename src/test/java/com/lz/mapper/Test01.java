15a85623ec6207012557c1e101c5d90a294578e0
2522399067 <2522399067@qq.com>
2024-06-27 11:30:07 +0800

    x修改01

--- src/test/java/com/lz/mapper/Test01.java
+++ src/test/java/com/lz/mapper/Test01.java
@@ -3,5 +3,13 @@ package com.lz.mapper;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("35a4sd35a4sd6a4sd6a54s");
+        System.out.println("35a4sd35a4sd6a4sd6a54s");
+        System.out.println("35a4sd35a4sd6a4sd6a54s");
+        System.out.println("35a4sd35a4sd6a4sd6a54s");
+        System.out.println("35a4sd35a4sd6a4sd6a54s");
+        System.out.println("35a4sd35a4sd6a4sd6a54s");
+        System.out.println("35a4sd35a4sd6a4sd6a54s");
+
+
     }
 }
